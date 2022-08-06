ac17a6fcf4b91d5ddcade4fb4e08adb6447fa14c
4meame <grasshatlee@gmail.com>
2022-08-06 17:36:24 +0800

    #提交<misc>Some New Settings and Scene

--- Assets/Scripts/Procedural Meshes/MeshGenerator.cs
+++ Assets/Scripts/Procedural Meshes/MeshGenerator.cs
@@ -4,25 +4,41 @@ using UnityEngine;
 
 [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
 public class MeshGenerator : MonoBehaviour {
+    public enum Type {
+        Quad,
+        Plane
+    }
+    public Type type;
     void OnEnable() {
-        var mesh = new Mesh {
+        switch (type) {
+            case Type.Quad:
+                GenerateQuad();
+                break;
+            case Type.Plane:
+                break;
+            default:
+                break;
+        }
+    }
+
+    void GenerateQuad() {
+        var mesh = new Mesh
+        {
             name = "Procedural Mesh"
         };
-        mesh.vertices = new Vector3[] { Vector3.zero, Vector3.right, Vector3.up, new Vector3(1f, 1f)};
+        mesh.vertices = new Vector3[] { Vector3.zero, Vector3.right, Vector3.up, new Vector3(1f, 1f) };
         //mesh.triangles = new int[] { 0, 1, 2 };
         //only visible when look the front face by default, we can turn this around by swaping the order of the second and third vertex indices
         mesh.triangles = new int[] { 0, 2, 1, 1, 2, 3 };
         mesh.normals = new Vector3[] { Vector3.back, Vector3.back, Vector3.back, Vector3.back };
         //tangents help shader construct the third axis
-        mesh.tangents = new Vector4[] { 
+        mesh.tangents = new Vector4[] {
             new Vector4(1f, 0f, 0f, -1f),
             new Vector4(1f, 0f, 0f, -1f),
             new Vector4(1f, 0f, 0f, -1f),
             new Vector4(1f, 0f, 0f, -1f)
         };
         mesh.uv = new Vector2[] { Vector2.zero, Vector2.right, Vector2.up, Vector2.one };
-        
         GetComponent<MeshFilter>().mesh = mesh;
     }
-
 }
