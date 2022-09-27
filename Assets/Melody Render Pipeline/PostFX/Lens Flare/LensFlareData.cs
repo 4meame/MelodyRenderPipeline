b2def083b27992222160b9ba17604808c6062b1a
4meame <grasshatlee@gmail.com>
2022-09-27 22:10:43 +0800

    #提交<feature> Part Of Lens Flare

--- Assets/Melody Render Pipeline/PostFX/Lens Flare/LensFlareData.cs
+++ Assets/Melody Render Pipeline/PostFX/Lens Flare/LensFlareData.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 [System.Serializable]
 public enum LensFlareType
@@ -19,6 +20,7 @@ public enum LensFlareBlendMode
     Lerp
 }
 
+//defined how we spread the flare element when count > 1
 [System.Serializable]
 public enum LensFlareDistribution
 {
@@ -28,11 +30,139 @@ public enum LensFlareDistribution
 }
 
 
-public class LensFlareDataElement {
 
+public class LensFlareDataElement {
+    public LensFlareDataElement() {
+        visible = true;
+        localIntensity = 1.0f;
+        position = 0.0f;
+        positionOffset = new Vector2(0.0f, 0.0f);
+        angularOffset = 0.0f;
+        translationScale = new Vector2(1.0f, 1.0f);
+        lensFlareTexture = null;
+        uniformScale = 1.0f;
+        sizeXY = Vector2.one;
+        allowMultipleElement = false;
+        count = 5;
+        rotation = 0.0f;
+        tint = new Color(1.0f, 1.0f, 1.0f, 0.5f);
+        blendMode = LensFlareBlendMode.Additive;
+        autoRotate = false;
+        isFoldOpened = true;
+        flareType = LensFlareType.Circle;
+        distribution = LensFlareDistribution.Uniform;
+        lengthSpread = 1f;
+        colorGradient = new Gradient();
+        colorGradient.SetKeys(new GradientColorKey[] { new GradientColorKey(Color.white, 0.0f), new GradientColorKey(Color.white, 1.0f) },
+            new GradientAlphaKey[] { new GradientAlphaKey(1.0f, 0.0f), new GradientAlphaKey(1.0f, 1.0f) });
+        positionCurve = new AnimationCurve(new Keyframe(0.0f, 0.0f, 1.0f, 1.0f), new Keyframe(1.0f, 1.0f, 1.0f, -1.0f));
+        scaleCurve = new AnimationCurve(new Keyframe(0.0f, 1.0f), new Keyframe(1.0f, 1.0f));
+        uniformAngleCurve = new AnimationCurve(new Keyframe(0.0f, 0.0f), new Keyframe(1.0f, 0.0f));
+        //random
+        seed = 0;
+        intensityVariation = 0.75f;
+        positionVariation = new Vector2(1.0f, 0.0f);
+        scaleVariation = 1.0f;
+        rotationVariation = 180.0f;
+        //distortion
+        enableRadialDistortion = false;
+        targetSizeDistortion = Vector2.one;
+        distortionCurve = new AnimationCurve(new Keyframe(0.0f, 0.0f, 1.0f, 1.0f), new Keyframe(1.0f, 1.0f, 1.0f, -1.0f));
+        distortionRelativeToCenter = false;
+        //parameters for procedural
+        fallOff = 1.0f;
+        edgeOffset = 0.1f;
+        sdfRoundness = 0.0f;
+        sideCount = 6;
+        inverseSDF = false;
+    }
+
+    public bool visible;
+    public float position;
+    public Vector2 positionOffset;
+    public float angularOffset;
+    public Vector2 translationScale;
+    [Min(0), SerializeField, FormerlySerializedAs("localIntensity")]
+    float m_LocalIntensity;
+    public float localIntensity {
+        get => m_LocalIntensity;
+        set => m_LocalIntensity = Mathf.Max(0, value);
+    }
+    public Texture lensFlareTexture;
+    public float uniformScale;
+    public Vector2 sizeXY;
+    public bool allowMultipleElement;
+    [Min(1), SerializeField, FormerlySerializedAs("count")]
+    int m_Count;
+    public int count {
+        get => m_Count;
+        set => m_Count = Mathf.Max(1, value);
+    }
+    public bool preserveAspectRatio;
+    public float rotation;
+    public Color tint;
+    public LensFlareBlendMode blendMode;
+    public bool autoRotate;
+    public LensFlareType flareType;
+    public bool modulateByLightColor;
+    public LensFlareDistribution distribution;
+    public float lengthSpread;
+    public AnimationCurve positionCurve;
+    public AnimationCurve scaleCurve;
+    public int seed;
+    public Gradient colorGradient;
+    [Range(0, 1), SerializeField, FormerlySerializedAs("intensityVariation")]
+    float m_IntensityVariation;
+    public float intensityVariation {
+        get => m_IntensityVariation;
+        set => m_IntensityVariation = Mathf.Max(0, value);
+    }
+    public Vector2 positionVariation;
+    public float scaleVariation;
+    public float rotationVariation;
+    public bool enableRadialDistortion;
+    //target size used on the edge of the screen
+    public Vector2 targetSizeDistortion;
+    public AnimationCurve distortionCurve;
+    //if true the distortion is relative to center of the screen otherwise relative to lensFlare source screen position
+    public bool distortionRelativeToCenter;
+    [Range(0, 1), SerializeField, FormerlySerializedAs("fallOff")]
+    float m_FallOff;
+    //fall of the gradient used for the procedural flare
+    public float fallOff {
+        get => m_FallOff;
+        set => m_FallOff = Mathf.Clamp01(value);
+    }
+    [Range(0, 1), SerializeField, FormerlySerializedAs("edgeOffset")]
+    float m_EdgeOffset;
+    //gradient offset used for the procedural flare
+    public float edgeOffset {
+        get => m_EdgeOffset;
+        set => m_EdgeOffset = Mathf.Clamp01(value);
+    }
+    [Min(3), SerializeField, FormerlySerializedAs("sideCount")]
+    int m_SideCount;
+    //side count of the regular polygon generated
+    public int sideCount {
+        get => m_SideCount;
+        set => m_SideCount = Mathf.Max(3, value);
+    }
+
+    [Range(0, 1), SerializeField, FormerlySerializedAs("sdfRoundness")]
+    float m_SdfRoundness;
+    //roundness of the polygon flare (0: Sharp Polygon, 1: Circle).
+    public float sdfRoundness {
+        get => m_SdfRoundness;
+        set => m_SdfRoundness = Mathf.Clamp01(value);
+    }
+    public bool inverseSDF;
+    public float uniformAngle;
+    public AnimationCurve uniformAngleCurve;
+    [SerializeField]
+    bool isFoldOpened;
 }
 
-[CreateAssetMenu(menuName = "Rendering/LensFlare")]
+[CreateAssetMenu(menuName = "Rendering/Lens Flare")]
 public class LensFlareData : ScriptableObject {
     public LensFlareData() {
         elements = null;
---
+++ Assets/Melody Render Pipeline/Shaders/LensFlareCommon.hlsl
@@ -0,0 +1,148 @@
+﻿#ifndef MELODY_LENS_FLARE_COMMON_INCLUDED
+#define MELODY_LENS_FLARE_COMMON_INCLUDED
+
+#include "Packages/com.unity.render-pipelines.core/ShaderLibrary/Filtering.hlsl"
+#include "Packages/com.unity.render-pipelines.core/ShaderLibrary/Random.hlsl"
+#include "Packages/com.unity.render-pipelines.core/ShaderLibrary/Sampling/Sampling.hlsl"
+
+struct AttributesLensFlare {
+	uint vertexID : SV_VERTEXID;
+#ifndef FLARE_PREVIEW
+	UNITY_VERTEX_INPUT_INSTANCE_ID
+#endif
+};
+
+struct VaryingsLensFlare {
+	float4 positionCS : SV_POSITION;
+	float2 texcoord : VAR_TEXCOORD;
+	float occlusion : VAR_OCCLUSION;
+#ifndef FLARE_PREVIEW
+	UNITY_VERTEX_OUTPUT_STEREO
+#endif
+};
+
+TEXTURE2D(_FlareTex);
+SAMPLER(sampler_FlareTex);
+#if defined(MRP_FLARE) && defined(FLARE_OCCLUSION)
+TEXTURE2D_X(_FlareOcclusionTex);
+SAMPLER(sampler_FlareOcclusionTex);
+#endif
+
+float4 _FlareColorValue;
+//x: localCos0, y: localSin0, zw: PositionOffsetXY
+float4 _FlareData0;
+//x: OcclusionRadius, y: OcclusionSampleCount, z: ScreenPosZ, w: ScreenRatio
+float4 _FlareData1;
+//xy: ScreenPos, zw: FlareSize
+float4 _FlareData2;
+//x: Allow Offscreen, y: Edge Offset, z: Falloff, w: invSideCount
+float4 _FlareData3;
+//x: SDF Roundness, y: Poly Radius, z: PolyParam0, w: PolyParam1
+float4 _FlareData4;
+
+#ifdef FLARE_PREVIEW
+float4 _FlarePreviewData;
+#define _ScreenSize         _FlarePreviewData.xy;
+#define _FlareScreenRatio   _FlarePreviewData.z;
+#endif
+
+//occlusion index calculated in compute shader
+float4 _FlareOcclusionIndex;
+
+#define _FlareColor             _FlareColorValue
+
+#define _LocalCos0              _FlareData0.x
+#define _LocalSin0              _FlareData0.y
+#define _PositionTranslate      _FlareData0.zw
+
+#define _OcclusionRadius        _FlareData1.x
+#define _OcclusionSampleCount   _FlareData1.y
+#define _ScreenPosZ             _FlareData1.z
+
+#ifndef _FlareScreenRatio
+#define _FlareScreenRatio       _FlareData1.w
+#endif
+
+#define _ScreenPos              _FlareData2.xy
+#define _FlareSize              _FlareData2.zw
+
+#define _OcclusionOffscreen     _FlareData3.x
+#define _FlareEdgeOffset        _FlareData3.y
+#define _FlareFalloff           _FlareData3.z
+#define _FlareShapeInvSide      _FlareData3.z
+
+#define _FlareSDFRoundness      _FlareData4.x
+#define _FlareSDFPolyRadius     _FlareData4.y
+#define _FlareSDFPolyParam0     _FlareData4.z
+#define _FlareSDFPolyParam1     _FlareData4.w
+
+float2 Rotate(float2 v, float cos0, float sin0) {
+	return float2(v.x * cos0 - v.y * sin0,
+		v.x * sin0 + v.y * cos0);
+}
+
+float GetLinearDepthValue(float2 uv) {
+#if defined(MRP_FLARE) || defined(FLARE_PREVIEW)
+	float depth = LOAD_TEXTURE2D_X_LOD(_CameraDepthTexture, uint2(uv * _ScreenSize.xy), 0).x;
+#else
+	float depth = LOAD_TEXTURE2D_X_LOD(_CameraDepthTexture, uint2(uv * GetScaledScreenParams().xy), 0).x;
+#endif
+	return LinearEyeDepth(depth, _ZBufferParams);
+}
+
+float GetOcclusion(float ratio) {
+	if (_OcclusionSampleCount == 0.0) {
+		return 1.0f;
+	}
+	float contribute = 0.0f;
+	float sample_contribute = 1.0f / _OcclusionSampleCount;
+	float2 ratioScale = float2(1.0f / ratio, 1.0);
+	for (unit i = 0; i < (uint)_OcclusionSampleCount; i++) {
+		float2 direction = _OcclusionRadius * SampleDiskUniform(Hash(2 * i + 0), Hash(2 * i + 1));
+		float2 position = _ScreenPos + direction;
+		//[-1,1] -> [0,1]
+		position.xy = position * 0.5 + 0.5;
+#ifdef UNITY_UV_STARTS_AT_TOP
+		position.y = 1.0f - position.y;
+#endif
+		if (all(position >= 0) && all(position <= 1)) {
+			float depth0 = GetLinearDepthValue(position);
+#if defined(UNITY_REVERSED_Z)
+			if (depth0 > _ScreenPosZ)
+#else
+			if (depth0 < _ScreenPosZ)
+#endif
+				contribute += sample_contribute;
+		}
+		else if (_OcclusionOffscreen > 0.0f) {
+			contribute += sample_contribute;
+		}
+	}
+	return contribute;
+}
+
+VaryingsLensFlare vertOcclusion(AttributesLensFlare input, uint instanceID : SV_INSTANCEID) {
+	VaryingsLensFlare output;
+//Single Pass Instanced
+	UNITY_SETUP_INSTANCE_ID(input);
+	float screenRatio = _FlareScreenRatio;
+	float2 quadPos = 2.0f * GetQuadVertexPosition(input.vertexID).xy - 1.0f;
+	float2 uv = GetQuadTexCoord(input.vertexID);
+	output.positionCS.xy = quadPos;
+	output.texcoord.xy = uv;
+	output.positionCS.z = 1.0f;
+	output.positionCS.w = 1.0f;
+	float occlusion = GetOcclusion(screenRatio);
+	if (_OcclusionOffscreen < 0.0f && // No lens flare off screen
+		(any(_ScreenPos.xy < -1) || any(_ScreenPos.xy >= 1)))
+		occlusion = 0.0f;
+	output.occlusion = occlusion;
+	return output;
+}
+
+float4 fragOcclusion(VaryingsLensFlare input) : SV_TARGET {
+	return float4(input.occlusion.xxx, 1.0f);
+}
+
+
+#endif
