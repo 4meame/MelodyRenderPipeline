475322605695ed011183fc1d6c114f0031780b7f
4meame <grasshatlee@gmail.com>
2022-10-21 00:22:18 +0800

    #提交<feature> Part Of Depth Of Field

--- Assets/Melody Render Pipeline/Shaders/DepthOfFieldUtils.hlsl
+++ Assets/Melody Render Pipeline/Shaders/DepthOfFieldUtils.hlsl
@@ -1,7 +1,7 @@
 ﻿#ifndef MELODY_DEPTH_OF_FIELD_UTILS_INCLUDED
 #define MELODY_DEPTH_OF_FIELD_UTILS_INCLUDED
 
-// Input textures
+//input textures
 TEXTURE2D(_InputTexture);
 TEXTURE2D(_InputCoCTexture);
 TEXTURE2D(_MinMaxTile);
@@ -25,7 +25,7 @@ TEXTURE2D(_MinMaxTile);
 #define BN_RAND_OFFSET 5
 #define RngStateType uint
 
-// Converts unsigned integer into float int range <0; 1) by using 23 most significant bits for mantissa
+//converts unsigned integer into float int range <0; 1) by using 23 most significant bits for mantissa
 float UintToNormalizedFloat(uint x) {
     return asfloat(0x3f800000 | (x >> 9)) - 1.0f;
 }
@@ -53,11 +53,13 @@ float RandomFloat01(inout RngStateType state, uint dimension) {
 #define BladeCount          _Params3.z
 #define Rotation            _Params3.w
 
+//accumData contain each bucket which accumulates Color, CoC and weight, starting from largest gather one ring of samples at a time
 struct AccumData {
-    float4 color;
-    float alpha;
-    float destAlpha;
+    float3 color;
     float CoC;
+    float weight;
+    float alpha;
+    float blendFactor;
 };
 
 struct DoFTile {
@@ -66,13 +68,14 @@ struct DoFTile {
     int numSamples;
 };
 
+//data of per sample of rings
 struct SampleData {
     float4 color;
     float CoC;
 };
 
 float GetSampleWeight(float cocRadius) {
-#ifdef PHYSICAL_WEIGHTS
+#if defined(PHYSICAL_WEIGHTS)
     //√2/2
     float pixelRadius = 0.7071f;
     float radius = max(pixelRadius, abs(cocRadius));
@@ -102,7 +105,7 @@ float4 GetColorSample(float2 texelCoord, float lod) {
 
 void LoadTileData(float2 texelCoord, SampleData centerSample, float rings, inout DoFTile tileData) {
     float4 cocRanges = LOAD_TEXTURE2D_LOD(_MinMaxTile, ResScale * texelCoord / TILE_RES, 0);
-    //NOTE : for the far-field, we don't need to search further than than the central CoC, if there is a larger CoC that overlaps the central pixel then it will have greater depth
+    //NOTE : for the far-field, we don't need to search further than the central CoC, if there is a larger CoC that overlaps the central pixel then it will have greater depth
     tileData.maxRadius = max(2 * abs(centerSample.CoC), -cocRanges.w) * OneOverResScale;
     //detect tiles than need more samples
     tileData.numSamples = rings;
@@ -117,7 +120,7 @@ void LoadTileData(float2 texelCoord, SampleData centerSample, float rings, inout
     if (cocRanges.w != 0 && cocRanges.y == 0) {
         //if there is no far field, then compute a splitting threshold that puts fg and bg in the near field
         //don't want any layers that span both the near and far field (CoC < 0 & CoC > 0)
-        tileData.layerBorder = (/*cocRanges.z*/ 0 + cocRanges.w) / 2;
+        tileData.layerBorder = (cocRanges.z + cocRanges.w) / 2;
     }
 #endif
 }
@@ -136,4 +139,101 @@ float2 PointOnNGon(float phi) {
 	return float2(u, v);
 }
 
+void ResolveColorAndAlpha(inout float4 outColor, inout float outAlpha, float4 defaultValue) {
+    outColor.xyz = outColor.w > 0 ? outColor.xyz / outColor.w : defaultValue.xyz;
+#if defined(ENABLE_ALPHA)
+    outAlpha = outColor.w > 0 ? outAlpha / outColor.w : defaultValue.w;
 #endif
+}
+
+//accumlate sample data to a accumData struct
+void AccumulateSampleToRing(SampleData sampleData, float weight, inout AccumData accumData) {
+    accumData.color += sampleData.color.xyz * weight;
+    accumData.CoC += abs(sampleData.CoC) * weight;
+    accumData.weight += weight;
+#if defined(ENABLE_ALPHA)
+    accumData.alpha += sampleData.color.w * weight;
+#endif
+}
+
+void AccumulateCenterSample(SampleData centerSample, inout AccumData accumData) {
+    float centerWeight = GetSampleWeight(centerSample.CoC);
+    accumData.color = accumData.color * (1 - centerWeight) + centerWeight * centerSample.color.xyz;
+    accumData.weight = accumData.weight * (1 - centerWeight) + centerWeight;
+#if defined(ENABLE_ALPHA)
+    accumData.alpha = accumData.alpha * (1 - centerWeight) + centerWeight * centerSample.color.w;
+#endif
+}
+
+//accumlate data of 2 samples of a ring
+void AccumulateRingData(SampleData sampleData[2], SampleData centerSample, float sampleRadius, float borderRadius, float layerBorder, const bool isForeground, inout AccumData accumData, inout AccumData ringAccum) {
+    [unroll]
+    for (int k = 0; k < 2; k++) {
+        //saturate allows a small overlap between the layers, this helps conceal any continuity artifacts due to differences in sorting
+        float w = saturate(sampleData[k].CoC - layerBorder);
+        float layerWeight = isForeground ? 1.0 - w : w;
+        float CoC = abs(sampleData[k].CoC);
+        float sampleWeight = GetSampleWeight(CoC);
+        float visibility = step(0.0, CoC - sampleRadius);
+        //check if the sample belongs to the current bucket
+        float borderWeight = saturate(CoC - borderRadius);
+#ifndef RING_OCCLUSION
+        //maybe artifacts in NGon
+        borderWeight = 0;
+#endif
+        float weight = layerWeight * visibility * sampleWeight;
+        AccumulateSampleToRing(sampleData[k], borderWeight * weight, accumData);
+        AccumulateSampleToRing(sampleData[k], (1.0 - borderWeight) * weight, ringAccum);
+    }
+}
+
+//bucketData is previous accumeulated data and ringData is the current bucketData
+void AccumulateBucketData(float numSamples, const bool isNearField, AccumData ringData, inout AccumData bucketData) {
+    if (ringData.weight == 0) {
+        // nothing to accumulate
+        return;
+    }
+    float currAvgCoC = ringData.weight > 0 ? ringData.CoC * rcp(ringData.weight) : 0;
+    float prevAvgCoC = bucketData.weight > 0 ? bucketData.CoC * rcp(bucketData.weight) : 0;
+    float occlusionCoC = saturate(prevAvgCoC - currAvgCoC);
+    float normCoC = ringData.CoC * rcp(ringData.weight);
+    float ringOpacity = saturate(ringData.weight * rcp(GetSampleWeight(normCoC)) * rcp(numSamples));
+    //near-field is the region where CoC > 0. In this case sorting is reversed
+    if (isNearField) {
+        const float occlusionWeight = 0.5;
+        occlusionCoC = occlusionWeight * (1 - saturate(currAvgCoC - prevAvgCoC));
+        // front-to-back blending
+        float blend = 1.0;
+#if defined(RING_OCCLUSION)
+        blend = bucketData.blendFactor;
+#endif
+        bucketData.color += blend * ringData.color;
+        bucketData.CoC += blend * ringData.CoC;
+        bucketData.weight += blend * ringData.weight;
+        bucketData.alpha += blend * ringData.alpha;
+        bucketData.blendFactor *= saturate(1 - 2 * ringOpacity * occlusionCoC);
+    } else {
+        // back-to-front blending
+        float blend = 0.0;
+#if defined(RING_OCCLUSION)
+        blend = (bucketData.weight > 0.0) ? ringOpacity * occlusionCoC : 1.0;
+#endif
+        bucketData.color = bucketData.color * (1.0 - blend) + ringData.color;
+        bucketData.CoC = bucketData.CoC * (1.0 - blend) + ringData.CoC;
+        bucketData.weight = bucketData.weight * (1.0 - blend) + ringData.CoC;
+        bucketData.alpha = bucketData.alpha * (1.0 - blend) + ringData.alpha;
+    }
+}
+
+void DoFGatherRings(PositionInputs posInputs, DoFTile tileData, SampleData centerSample, out float4 color, out float alpha) {
+    AccumData bgAccumData, fgAccumData;
+    ZERO_INITIALIZE(AccumData, bgAccumData);
+    ZERO_INITIALIZE(AccumData, fgAccumData);
+    //layers in the near field are using front-to-back accumulation (so start with a dest alpha of 1, ignored if in the far field)
+    fgAccumData.blendFactor = 1;
+    bgAccumData.blendFactor = 1;
+
+}
+
+#endif
+
